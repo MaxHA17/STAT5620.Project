687c19d0250af8f0a461ae273e7188365bae93ae
Max <max.henry-adams@dal.ca>
2025-03-05 14:46:04 -0400

    Added project description and a documented file containing the raw data

---
+++ R/RawData.R
@@ -0,0 +1,33 @@
+#' Grey seal diet and reproductive success data
+#'
+#' The RawData file contains all of the raw diet and reproductive success data to be used in this project:
+#'
+#' - **MomID**: Unique identifier for the mother.
+#' - **Year**: Deployment year.
+#' - **Dietary_energy_density**: Energy density (kJ/g) of the female diet calculated from the average energy content per gram of prey body tissue.
+#' - **Diet.diversity**: Standardized Shannon-Weaver diversity index to quantify individual diet diversity.
+#' - **Dominant_prey_species**: The prey species making up the greatest proportion of the mothers diet.
+#' - **Mom.Age**: Age of the mother in years.
+#' - **Deploy.Mass**: The mothers mass (in kg) at initial deployment before the foraging period.
+#' - **Recover.Mass**: The mass of the mother (in kg) when she returns to the breeding colony to give birth to her pup.
+#' - **Mass.change**: The mothers recovery mass minus her deployment mass.
+#' - **Pup.age.at.mom.mass.measurement**: If known, the age of the pup (in days) when the mother was found and samples with her pup.
+#' - **PupID**: Unique identifier for the individual pup.
+#' - **Pup.sex**: A binary character representing the sex of male (1) or female (2) pups.
+#' - **Pup.birth.Date**: If known, the date the pup was born.
+#' - **Pup.birth..Mass**: The mass (in kg) of the pup at birth.
+#' - **Pup.Age.at.Weaning**: The age (in days) of the pup when the mother terminated lactation and the pup was weaned.
+#' - **Pup.Wean.Mass.date**: The date the pup was weaned.
+#' - **Pup.Wean.Mass**: The mass (in kg) of the pup at weaning.
+#' - **Lactation.duration**: The number of days between the pups birth date and weaning date.
+#'
+#' @format A data frame with 80 rows and 18 variables:
+#'
+#' @source Data from the Department of Fisheries and Oceans Canada grey usethis::use_data(raw_data, overwirte=TRUE)seal program
+#'
+#' @examples
+#' data(raw_data)
+#' summary(raw_data)
+
+#Load the data and add the data to the r package
+raw_data <- STAT5620_Data <- read.csv("C:/Users/Maxhe/OneDrive - Dalhousie University/Desktop/STAT5620/STAT5620_Data.csv")
